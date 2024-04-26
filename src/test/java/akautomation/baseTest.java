c8cec56c24e5ac350de5c6fe41acbf52bf8eb588
DESKTOP-AKFIOHB\Rayyan Tech <rcsmetx@gmail.com>
2024-04-27 00:51:50 +0200

    Added visit url, search query, enter key, click on link, assertion on title

--- src/test/java/akautomation/baseTest.java
+++ src/test/java/akautomation/baseTest.java
@@ -1,5 +1,8 @@
 package akautomation;
 
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -29,6 +32,27 @@ public class baseTest {
 		driver.manage().deleteAllCookies();
 		//visiting my Portfolio
 		driver.get("https://rcsmetx.wixsite.com/arsalanportfolio");
+		//wait for 3 seconds
+		Thread.sleep(3000);
+		//visit google
+		driver.get("https://google.com");
+		//rejecting cookies on google consent modal
+		if(driver.findElement(By.xpath("//*[text()= 'Alle ablehnen']")).isDisplayed()) {
+			driver.findElement(By.xpath("//*[text()= 'Alle ablehnen']")).click();
+		}
+		//searching query 
+		driver.findElement(By.name("q")).sendKeys("dummy login portal for testing");
+		//enter key after entering the query
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		//clicking on searched link
+		driver.findElement(By.xpath("//a[@href = 'https://practicetestautomation.com/practice-test-login/']")).click();
+		//getting page title
+		String ActualTitle = driver.getTitle();
+		//storing expected value in string variable
+		String ExpectedTitle = "Test Login | Practice Test Automation";
+		//comparing actual vs expected title
+		Assert.assertEquals(ExpectedTitle, ActualTitle);
+		Thread.sleep(5000);
 		//quit browser
 	 	driver.quit();
 				
