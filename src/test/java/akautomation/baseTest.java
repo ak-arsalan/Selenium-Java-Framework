d0a92d8842edd33d01db62af88760b55dfbd1b50
DESKTOP-AKFIOHB\Rayyan Tech <rcsmetx@gmail.com>
2024-04-28 16:55:06 +0200

    Added Extended Report Maven Repository

--- src/test/java/akautomation/baseTest.java
+++ src/test/java/akautomation/baseTest.java
@@ -3,6 +3,12 @@ package akautomation;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -14,7 +20,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class baseTest {
 	
 	ChromeDriver driver;
-	
+	//create the htmlReporter object 
+	ExtentSparkReporter htmlReporter = new ExtentSparkReporter("extentReport.html");
+	//create ExtentReports and attach reporter(s)
+	ExtentReports extent = new ExtentReports();
+	//creates a toggle for the given test, add all log events under it
+	ExtentTest test1 = extent.createTest("Java Maven TestNG Framework", "Here we go!");
+
 	@BeforeTest
 	public void Setup() {
 		
@@ -29,13 +41,15 @@ public class baseTest {
 		*/
 		
 		//Command for specific version of browser
-        //WebDriverManager.chromedriver().browserVersion("2.36").setup();
+	    //WebDriverManager.chromedriver().browserVersion("2.36").setup();
+		
+		extent.attachReporter(htmlReporter);
 		
 		//Setting up ChromeDriver
 		WebDriverManager.chromedriver().setup();
 		//Creating object with driver name
-        driver= new ChromeDriver();
-        //maximizing the browse window
+	    driver= new ChromeDriver();
+	    //maximizing the browse window
 		driver.manage().window().maximize();
 		//clearing the Cache
 		driver.manage().deleteAllCookies();
@@ -44,16 +58,20 @@ public class baseTest {
 	
 	@Test
 	public void actualTest() throws InterruptedException {
-		//Visit my portfolio
-		driver.get("https://rcsmetx.wixsite.com/arsalanportfolio");
+		
+		test1.log(Status.INFO, "Url opened");
+		//maximize the window 
 		//wait for 3 seconds
 		Thread.sleep(3000);
 		//visit google
 		driver.get("https://google.com");
+		test1.log(Status.INFO, "Url opened");
 		//rejecting cookies on google consent modal
 		if(driver.findElement(By.xpath("//*[text()= 'Alle ablehnen']")).isDisplayed()) {
 			driver.findElement(By.xpath("//*[text()= 'Alle ablehnen']")).click();
+			test1.log(Status.INFO, "closing google consent");
 		}
+		
 		//searching query 
 		driver.findElement(By.name("q")).sendKeys("dummy login portal for testing");
 		//enter key after entering the query
@@ -61,11 +79,13 @@ public class baseTest {
 		//clicking on searched link
 		driver.findElement(By.xpath("//a[@href = 'https://practicetestautomation.com/practice-test-login/']")).click();
 		//getting page title
+		test1.log(Status.INFO, "Search site for testing passed");
 		String ActualTitle = driver.getTitle();
 		//storing expected value in string variable
 		String ExpectedTitle = "Test Login | Practice Test Automation";
 		//comparing actual vs expected title
 		Assert.assertEquals(ExpectedTitle, ActualTitle);
+		test1.log(Status.INFO, "Title matching passed");
 		
 		//usecase 1: submit the form without filling any field
 		driver.findElement(By.id("submit")).click();
@@ -113,6 +133,10 @@ public class baseTest {
 	public void endTest() {
 		//quit browser
 	 	driver.quit();
+	 	test1.pass("closed the browser");
+	 	test1.info("test completed");
+	 	//write results into the file
+	 	extent.flush();
 	}
 				
 }
